168a18dbd6ea1739a8009aced81d4dc37b816c28
vinhlap004 <50997855+vinhlap004@users.noreply.github.com>
2019-11-25 18:34:50 +0700

    create file .env to congfig port and db_host

--- model/publishers.model.js
+++ model/publishers.model.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 //1.require mongoose
 var mongoose = require('mongoose');
 
 //2.connect
-if (mongoose.connect('mongodb+srv://linh796:linh796@cluster0-lbsr0.mongodb.net/bookshop?retryWrites=true&w=majority')){
+if (mongoose.connect(process.env.DB_HOST,{useNewUrlParser:true,useUnifiedTopology: true })){
 	console.log('connected to database\n');
 }
 
