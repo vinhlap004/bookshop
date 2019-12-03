7e022c2124501f3e6a5e4788f21a0ef217402156
vinhlap004 <50997855+vinhlap004@users.noreply.github.com>
2019-12-03 20:43:55 +0700

    Revert "Cấu trúc lại file"
    This reverts commit 550975570f9a3132de4f1d5ed4b34cec9421fa8c.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.DB_HOST,{useNewUrlParser:true,useUnifiedTopology: t
 
 
 // passport
-require('./config/passport')(passport);
+require('./controllers/passport')(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -69,7 +69,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', indexRouter);
-app.use('/', usersRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const controllerUser = require('../controllers/user.controller');
 
 /* GET users listing. */
-
-router.get('/login',function(req, res, next) {
-  res.render('login');
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
-
-router.post('/login',controllerUser.login);
-
-router.get('/logout',controllerUser.logout);
-
-router.get('/register',function(req, res, next) {
-  res.render('register');
-});
-
-router.post('/register',controllerUser.register);
 
 module.exports = router;
